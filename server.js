4ae7f0a1f8e611dec3abfdff634a856ec45920e2
alarrabee <alexlarrabee10@gmail.com>
2024-07-24 06:39:31 -0500

    set up user model and create virtual to get friend count

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+
+//sets up the express app
 const app = express();
 const port = process.env.PORT || 3001;
 
 
-//middleware
+//middleware to handle json and url-encoded (form) data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+//middleware for handling routes
 app.use(routes);
 
 
